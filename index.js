19ab3030c7e519a9f64757a9d04b28a0f3593e48
CheloDev <warsoluciones@gmail.com>
2024-08-16 10:29:18 -0300

    data. json funcionando

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 //creo un server con express
 const express = require("express");
 const server = express();
+
+//ruta para acceder a la data
+const data = require('./data.json')
+
 //Activar variable de entorno con dotenv
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 //ruta raiz
-server.get("/", (req, res) => {
+server.get("/main", (req, res) => {
   res.send("Tienda Inicio");
 });
+
+//ruta productos
+server.get('/productos', (req, res)=>{
+    res.json(data)
+})
 
 //Iniciar servidor
 server.listen(PORT, () => {
