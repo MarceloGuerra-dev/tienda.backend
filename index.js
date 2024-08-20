52e23fc735bc510c384e69b8fbe2a7ec24d2b942
CheloDev <warsoluciones@gmail.com>
2024-08-20 11:24:55 -0300

    Se agrego la ruta fronted para cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 //creo un server con express
 const express = require("express");
 const server = express();
-const routerProductos = require('./src/routes/indexRoutes')
+const routerProductos = require("./src/routes/indexRoutes");
 
 //Activar variable de entorno con dotenv
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+//Permisos de CORS
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  next();
+ });
 
 //ruta raiz
 server.get("/main", (req, res) => {
@@ -13,7 +19,7 @@ server.get("/main", (req, res) => {
 });
 
 //ruta productos
-server.use('/main', routerProductos)
+server.use("/main", routerProductos);
 
 //Iniciar servidor
 server.listen(PORT, () => {
